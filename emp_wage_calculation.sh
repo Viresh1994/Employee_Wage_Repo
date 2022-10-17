67a3c79c73b3965706de26b81ce080b23d1e3189
Viresh1994 <vireshanadakatni@gmail.com>
2022-10-17 22:48:53 +0530

    [Viresh]Integrated UC3 branch added part time

--- emp_wage_calculation.sh
+++ emp_wage_calculation.sh
@@ -4,14 +4,16 @@ echo " Welcome to Employee Wage Computation"
 
 perHourSalary=20
 fullDayHours=8
+partTimeHours=4
 attendance=$((RANDOM%2))
-isPresent=1
 workHours=0
 salperDay=0
-if [ $attendance -eq $isPresent ]
-then
-echo "Present"
-workHours=8
-else
-echo "Absent"
-fi
+case $attendance in
+		0) echo "absent";;
+		1) echo "Present"
+					workHours=8;;
+		2) echo "part time present"
+					workHours=4;;
+esac
+
+echo "Today earning="$(($workHours*$perHourSalary))
