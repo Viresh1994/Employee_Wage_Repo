2e14dc56710f1860159c471d00444ce537b1d04e
Viresh1994 <vireshanadakatni@gmail.com>
2022-10-17 22:59:59 +0530

    [Viresh] Integrated UC4 wage calculation for month

--- emp_wage_calculation.sh
+++ emp_wage_calculation.sh
@@ -5,15 +5,26 @@ echo " Welcome to Employee Wage Computation"
 perHourSalary=20
 fullDayHours=8
 partTimeHours=4
+
 attendance=$((RANDOM%2))
 workHours=0
-salperDay=0
+totalSalary=0
+perDaySalary=0
+
+for ((day=1; day<=20; day++))
+do
+attendance=$((RANDOM%3))
 case $attendance in
-		0) echo "absent";;
+		0) echo "absent"
+					workHours=0;;
 		1) echo "Present"
 					workHours=8;;
 		2) echo "part time present"
 					workHours=4;;
 esac
 
+perDaySalary=$((workHours*$perHourSalary))
+totalSalary=$((totalSalary+$perDaySalary))
+done
+echo "Month earning="$totalSalary
 echo "Today earning="$(($workHours*$perHourSalary))
