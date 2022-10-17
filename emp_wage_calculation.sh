bb12890a7cf4c3a11c7bea246651f95ee7b1e563
Viresh1994 <vireshanadakatni@gmail.com>
2022-10-17 22:42:35 +0530

    [Viresh]Integrated with UC2 daily wage calculation

--- emp_wage_calculation.sh
+++ emp_wage_calculation.sh
@@ -2,11 +2,16 @@
 
 echo " Welcome to Employee Wage Computation"
 
+perHourSalary=20
+fullDayHours=8
 attendance=$((RANDOM%2))
 isPresent=1
-if [ $isPresent -eq $attendance ]
+workHours=0
+salperDay=0
+if [ $attendance -eq $isPresent ]
 then
 echo "Present"
+workHours=8
 else
 echo "Absent"
 fi
